ab5f9b1d0a36ab12b72c94143edc3c488a3af25b
Radu Luca <121110089+NotMuha1@users.noreply.github.com>
2025-04-17 19:23:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Placeholder for future interactivity
+console.log("Beach, Please! website loaded.");
+console.log("Made with ❤️ by Muha");
+console.log("© 2025 Beach,Please! All rights reserved.");
+
+// ©️ NotMuha - All Rights Reserved ©️
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Proprietary and confidential.
